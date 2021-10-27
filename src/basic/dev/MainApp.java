dd464711bba15690ad1c57ef175787f2895ee8b3
Vu <hoaivunguyen076@gmail.com>
2021-10-27 15:19:55 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,49 @@
+package basic.dev;
+
+public class MainApp {
+	public static void main(String[] args) {
+		/**
+		 * khai bao thong tin sinh vien
+		 */
+		String ht = "Nguyen Hoai Vu";
+		int msv = 2160739;
+		int cmnd = 191918425;
+		String tenLop = "21CDTM";
+		String tenTruong = "Cao Dang Cong Nghiep Hue";
+		float diemTrungBinh = 4.5f;
+		
+		System.out.println("Ho va ten: " + ht);
+		System.out.println("Ma Sinh Vien: " + msv);
+		System.out.println("Chung Minh Nhan Dan: " + cmnd);
+		System.out.println("Ten Lop: " + tenLop);
+		System.out.println("Ten Truong: " + tenTruong);
+		System.out.println("Diem Trung Binh: " + diemTrungBinh);
+		
+		/**
+		 * khai bao thong tin cong nhan
+		 */
+		String ten = "Nguyen Hoai Vu";
+		String diaChi = "kiet 47 Luong Van Can";
+		String sdt = "0932571094";
+		float hsl = 5.3f;
+		
+		System.out.println("Ho va ten: " + ten);
+		System.out.println("Dia chi: " + diaChi);
+		System.out.println("So Dien Thoai: " + sdt);
+		System.out.println("He So Luong: " + hsl);
+		
+		/**
+		 * khai bao thong tin ve chiec xe car
+		 */
+		String tenxe = "vinfast";
+		String hang = "vingr";
+		float gia = 5.0f;
+		String mauSon = "red";
+		
+		System.out.println("Ten Xe: " + tenxe);
+		System.out.println("Hang xe: " + hang);
+		System.out.println("Gia xe: " + gia);
+		System.out.println("Mau xe: " + mauSon);
+				
+	}
+}
